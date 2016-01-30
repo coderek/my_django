918253f0c649b04540a6471328c13fb71d8f9e58
coderek <derek@chango.com>
2016-01-30 12:31:37 -0500

    Use hostname for db

--- my_django/settings.py
+++ my_django/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'my_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '',
+        'HOST': 'derekzeng.me',
         'NAME': 'my_django',
         'PASSWORD': 'zengqiang',
         'USER': 'root',
