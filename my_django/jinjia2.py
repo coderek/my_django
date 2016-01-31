4d68e4490fc74efb19dc50bb3e05dfefeb9f523a
coderek <derek@chango.com>
2016-01-31 11:16:48 -0500

    Remove unnecessaries

--- my_django/jinjia2.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import  # Python 2 only
-from jinja2 import Environment
-
-
-def environment(**options):
-    env = Environment(**options)
-    env.globals.update({
-    })
-    return env
