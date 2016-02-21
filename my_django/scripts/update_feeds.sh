803eb1b363fd39326b58c277875cc7df4866ec01
coderek <derek@chango.com>
2016-02-20 23:11:23 -0500

    Update scripts

--- my_django/scripts/update_feeds.sh
+++ my_django/scripts/update_feeds.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source /root/django/bin/activate && /root/my_django/manage.py update_feeds >> /var/log/django/debug.log 2>&1
+source /root/django/bin/activate && /root/my_django/manage.py update_feeds
