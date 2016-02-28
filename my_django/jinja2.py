3ea10c36d55a3ddb7aa34ab498119a7fe7914a4f
coderek <derek@chango.com>
2016-02-27 20:23:37 -0500

    Add scss compiler

--- my_django/jinja2.py
+++ my_django/jinja2.py
@@ -4,12 +4,26 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 
 from jinja2 import Environment
+from markupsafe import Markup
+
+
+def static(raw_path):
+    wrapper = '{}'
+    # '{}{}'.format(settings.STATIC_URL, raw_path)
+    path = staticfiles_storage.url(raw_path)
+    if path.endswith('.scss'):
+        wrapper = '<link rel="stylesheet" type="text/x-scss" href="{}">'
+    if path.endswith('.css'):
+        wrapper = '<link rel="stylesheet" type="text/css" href="{}">'
+    if path.endswith('.js'):
+        wrapper = '<script src="{}"></script>'
+    return Markup(wrapper.format(path))
 
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage.url,
         'url': reverse,
+        'static': static,
     })
     return env
