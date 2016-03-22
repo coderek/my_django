b7afe15805b8d8173d67c0e0d2c30bf0774a1ea5
coderek <derek@chango.com>
2016-03-21 21:58:48 -0400

    Update title

--- my_django/urls.py
+++ my_django/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.contrib import admin
 from blog.views import HomeView
 
+
+admin.site.site_header = 'Yellow Ribbon Project'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('blog.urls')),
